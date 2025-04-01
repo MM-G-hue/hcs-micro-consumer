62fa8190f581833336576c38068c404bc81625c3
MM-G-hue <marijn.martin@hotmail.com>
2025-04-01 14:15:31 +0200

    add memory logging

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const amqp = require('amqplib/callback_api');
 const { InfluxDB } = require('@influxdata/influxdb-client');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 
 // RabbitMQ Connection Details
@@ -118,6 +120,29 @@ function createChannel(connection) {
         );
     });
 }
+
+function logMemoryUsage(interval = 5000, filename = 'memory_log_consumer.csv') {
+    const filePath = path.join(__dirname, filename);
+    const headers = 'Timestamp,RSS (bytes),Heap Total (bytes),Heap Used (bytes),External (bytes),Array Buffers (bytes)\n';
+    
+    // Write headers if file doesn't exist
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, headers);
+    }
+
+    setInterval(() => {
+        const mem = process.memoryUsage();
+        const timestamp = new Date().toISOString();
+        const data = `${timestamp},${mem.rss},${mem.heapTotal},${mem.heapUsed},${mem.external},${mem.arrayBuffers}\n`;
+        
+        fs.appendFile(filePath, data, (err) => {
+            if (err) console.error('Error writing to log file:', err);
+        });
+    }, interval);
+}
+
+logMemoryUsage(); // Logs memory every 5 seconds
+
 
 // Start consuming messages from RabbitMQ
 connectRabbitMQ();
